2d1fc1783afdd1daa22867733d3aff7365305854
whosensei <sukunagojo965@gmail.com>
2025-06-11 01:44:16 +0530

    Refactored media generation hook to support multiple API endpoints

--- hooks/use-mediageneration.tsx
+++ hooks/use-mediageneration.tsx
@@ -1,21 +1,43 @@
 import { useState } from "react";
 
 const useMediaGeneration = () => {
-    const [mediaUrl, setMediaUrl] = useState(null);
-    const [loading, setLoading] = useState(false);
-  
-    const generate = async (prompt:string) => {
-      setLoading(true);
-      const res = await fetch('/api/generate', {
-        method: 'POST',
-        body: JSON.stringify({ prompt }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      const { imageUrl } = await res.json();
-      setMediaUrl(imageUrl);
-      setLoading(false);
-    };
-  
-    return { mediaUrl, generate, loading };
+  const [mediaUrl, setMediaUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const callApi = async (endpoint: string, prompt: string) => {
+    setLoading(true);
+    const res = await fetch(endpoint, {
+      method: "POST",
+      body: JSON.stringify({ prompt }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const { imageUri } = await res.json();
+    setMediaUrl(imageUri);
+    setLoading(false);
   };
+
+  const generateheadshots = (prompt: string) =>
+    callApi("/api/flux-headshot", prompt);
+
+  const generateportraits = (prompt: string) =>
+    callApi("api/flux-portraits", prompt);
+
+  const generatehairstyles = (prompt: string) =>
+    callApi("/api/flux-hairstyle", prompt);
+
+  const generateimages = (prompt: string) =>
+     callApi("api/imagen-4", prompt);
+
+  const editimages = (prompt: string) =>
+    callApi("api/flux-kontext(edit)", prompt);
+
+  return {
+    mediaUrl,
+    loading,
+    generateheadshots,
+    generateportraits,
+    generatehairstyles,
+    generateimages,
+    editimages
+  };
+};
