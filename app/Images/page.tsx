6062690f593f3d7c8665fb6bd771bb9999ac23ba
whosensei <sukunagojo965@gmail.com>
2025-06-14 02:22:21 +0530

    fixed chatbar layout issues

--- app/Images/page.tsx
+++ app/Images/page.tsx
@@ -9,13 +9,13 @@ export default function ADcomponent() {
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="pb-3 font-semibold text-4xl tracking-tighter text-balance">
         What do you wish to create !
-        <SettingsProvider>
-          <div className="w-full max-w-2xl mx-auto">
-            <ChatInput />
-            <Buttonlist />
-          </div>
-        </SettingsProvider>
       </div>
+      <SettingsProvider>
+        <div className="w-full max-w-2xl mx-auto">
+          <ChatInput />
+          <Buttonlist />
+        </div>
+      </SettingsProvider>
     </div>
   );
 }
